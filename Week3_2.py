dbf5b9f27a9e53219c2009cddaf6932ddbe6e85e
Kith6437 <sothearith.kith@my.jcu.edu.au>
2026-06-03 16:39:01 +0700

    test

---
+++ Week3_2.py
@@ -0,0 +1,18 @@
+##import random
+ 
+#low = float(input("Enter the low number: "))
+#high = float(input("Enter the high number: "))
+ #
+##while high <= low:
+##    print("High must be greater than low. Try again.")
+##    high = float(input("Enter the high number: "))
+ 
+##n = random.randint(int(low), int(high))
+##print(f"\nn = {n}")
+##rint(":) " * n)
+
+
+def print_Line(number, char):
+    print(number * char)
+
+print_Line(10, "*")
